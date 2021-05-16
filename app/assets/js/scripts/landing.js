6097c17d45b895ee7be3313afe482e74e53aab7e
lucasboss45 <l4f4go@gmail.com>
2021-05-17 00:30:29 +0200

    Fix Runtime error on first launch cuz me dumb

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -1098,7 +1098,7 @@ function dlAsync(login = true){
                                     fs.readdirSync(paths.mods).forEach((file) => {
                                         // Prevent optifine to be deleted here because of Java Path issues
                                         // Shit patch but honestly I don't care, I don't have time to implement something better
-                                        if(file !== 'OptiFine.jar' && file !== 'MixinBootstrap.jar') {
+                                        if(file !== 'OptiFine.jar' && file !== 'MixinBootstrap.jar' && file !== "nicephore") {
                                             fs.unlinkSync(path.join(paths.mods, file))
                                         }
                                     })
